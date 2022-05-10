c7b70304443bd775dd3905aefd48c6fe6497ea8c
joaoalca <joao.alcaraz@sou.inteli.edu.br>
2022-05-10 18:22:17 -0300

    add new func

--- 03_AUT_EST_ENTREGA/Semana 4/Curriculo com js/scripts/scripts.js
+++ 03_AUT_EST_ENTREGA/Semana 4/Curriculo com js/scripts/scripts.js
@@ -1,7 +1,35 @@
 window.onload = function() {
+    
     let newParag = document.createElement("p");
-    let textParag = document.createTextNode("Segue as fotos abaixo:");
+    let textParag = document.createTextNode("Clique no botão abaixo para ver as imagens:");
     newParag.appendChild(textParag);
     let divAdd = document.getElementById("textojs");
     divAdd.appendChild(newParag);
-};
+
+}
+
+$(document).ready(function(){  
+
+    $("img").hide("fast")
+
+});
+
+$("button.highL").click(function() {
+
+    $("div.marca").css("background-color", "yellow").show("fast");
+    //$("img").slideDown("slow");
+
+});
+
+$("button.unhighL").click(function() {
+
+    $("div.marca").css("background-color", "").show("fast");
+    //$("img").slideUp("slow");
+
+});
+
+$("button.showImg").click(function() {
+
+    $("img").slideDown("slow");
+
+});
